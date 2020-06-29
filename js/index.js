2c25fcfbc3ded8f83603362562663fcb9d65f187
Aaron Wesolek <maxwes13@gmail.com>
2020-06-28 21:41:45 -0500

    finished MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,72 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+//navigation
+
+const anchors = document.querySelectorAll('nav a');
+anchors[0].textContent = siteContent["nav"]["nav-item-1"];
+anchors[1].textContent = siteContent["nav"]["nav-item-2"];
+anchors[2].textContent = siteContent["nav"]["nav-item-3"];
+anchors[3].textContent = siteContent["nav"]["nav-item-4"];
+anchors[4].textContent = siteContent["nav"]["nav-item-5"];
+anchors[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+//CTA
+const heading = document.querySelector('.cta-text h1');
+heading.textContent = siteContent["cta"]["h1"];
+
+const button = document.querySelector('.cta-text button');
+button.textContent = siteContent["cta"]["button"];
+
+const ctaLogo = document.querySelector('#cta-img');
+ctaLogo.src = siteContent["cta"]["img-src"];
+
+//main-content
+const headings = document.querySelectorAll('.text-content h4');
+headings[0].textContent = siteContent["main-content"]["features-h4"];
+headings[1].textContent = siteContent["main-content"]["about-h4"];
+headings[2].textContent = siteContent["main-content"]["services-h4"];
+headings[3].textContent = siteContent["main-content"]["product-h4"];
+headings[4].textContent = siteContent["main-content"]["vision-h4"];
+
+const paragraphs = document.querySelectorAll('.text-content p');
+paragraphs[0].textContent = siteContent["main-content"]["features-content"];
+paragraphs[1].textContent = siteContent["main-content"]["about-content"];
+paragraphs[2].textContent = siteContent["main-content"]["services-content"];
+paragraphs[3].textContent = siteContent["main-content"]["product-content"];
+paragraphs[4].textContent = siteContent["main-content"]["vision-content"];
+
+const midImg = document.querySelector('#middle-img');
+midImg.src = siteContent["main-content"]["middle-img-src"];
+
+//contact
+const contact = document.querySelector('.contact h4');
+contact.textContent = siteContent["contact"]["contact-h4"];
+
+const contactP = document.querySelectorAll('.contact p');
+contactP[0].textContent = siteContent["contact"]["address"];
+contactP[1].textContent = siteContent["contact"]["phone"];
+contactP[2].textContent = siteContent["contact"]["email"];
+
+//footer
+const footer = document.querySelector('footer p');
+footer.textContent = siteContent["footer"]["copyright"];
+
+//task 3
+const navColor = document.querySelectorAll('nav a');
+navColor.forEach(link => link.style.color = 'green');
+
+const nav1 = document.createElement('a');
+nav1.href = '#';
+nav1.textContent = 'Yup';
+const navigation = document.querySelector('header nav');
+navigation.prepend(nav1);
+
+const nav2 = document.createElement('a');
+nav2.href = '#';
+nav2.textContent = 'Here';
+navigation.appendChild(nav2);
+
